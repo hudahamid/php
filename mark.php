0f405da8960bda02228218bd2c8e7bbdc19a97cc
hudahamid <36269806+hudahamid@users.noreply.github.com>
2024-05-04 14:56:25 +0300

    Create mark.php

---
+++ mark.php
@@ -0,0 +1,10 @@
+<?php   
+    $marks = 69;       
+    if ($marks > 50) {
+        echo "Mark : $marks <br>";   
+        echo "Congratulations! You have passed";     
+    }     
+    else {
+        echo "Fail";     
+    }     
+?>  
