9e017eae897d3522a3866ea5bb2f2ac7350cdfc4
hudahamid <36269806+hudahamid@users.noreply.github.com>
2024-06-26 16:22:33 +0300

    Create increments.php

---
+++ increments.php
@@ -0,0 +1,26 @@
+<?php
+// Online PHP compiler to run PHP program online
+// Print "Try programiz.pro" message
+
+$x=10;
+echo ++$x;
+echo "\n";
+echo $x;
+echo "\n";
+$x=10;
+echo $x++;
+echo "\n";
+echo $x;
+echo "\n";
+$x=10;
+echo --$x;
+echo "\n";
+echo $x;
+echo "\n";
+$x=10;
+echo $x--;
+echo "\n";
+echo $x;
+echo "\n";
+
+?>
