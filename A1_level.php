6d4aa383a682f601e108cec1b748b4404c4b2d25
hudahamid <36269806+hudahamid@users.noreply.github.com>
2024-11-21 18:02:59 +0300

    Create A1_level.php

---
+++ A1_level.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!<br>";
+ 
+
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+// This is a single-line comment
+
+# This is also a single-line comment
+?> 
+</body>
+</html>
